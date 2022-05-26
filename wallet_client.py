de32c31f3a1edb8900b47c339945d09217673cf3
Lucas Mundim <lucas.mundim044@gmail.com>
2022-05-26 14:29:13 -0300

    add stub interactivity

--- wallet_client.py
+++ wallet_client.py
@@ -2,15 +2,18 @@ import grpc
 import wallet_routes_pb2
 import wallet_routes_pb2_grpc
 
-def wallet_get_balance(stub):
-    balance = stub.GetBalance(wallet_routes_pb2.Wallet(id="douglas_adams"))
+def wallet_get_balance(stub, wallet_id):
+    balance = stub.GetBalance(wallet_routes_pb2.Wallet(id=wallet_id))
     return balance.total_balance
 
 def run():
     with grpc.insecure_channel('localhost:42000') as channel:
         stub = wallet_routes_pb2_grpc.WalletRoutesStub(channel)
-        print("-------------- GetBalance --------------")
-        print(wallet_get_balance(stub))
+        while True:
+            wallet_id = input()
+            if wallet_id == 'F':
+                break
+            print(wallet_get_balance(stub, wallet_id))
 
 
 if __name__ == '__main__':
--- wallet_server.py
+++ wallet_server.py
@@ -5,11 +5,11 @@ import grpc
 
 class WalletRoutesServicer(wallet_routes_pb2_grpc.WalletRoutesServicer):
     def GetBalance(self, request, context):
-        balance = 42
+        balance = 42-int(request.id)
         if balance is None:
             return wallet_routes_pb2.Balance(total_balance=0)
         else:
-            return wallet_routes_pb2.Balance(total_balance=42)
+            return wallet_routes_pb2.Balance(total_balance=balance)
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
