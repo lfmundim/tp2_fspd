c46cbdc0ed2f93faca1cd2a135b188a4c28df61e
Lucas Mundim <lucas.mundim044@gmail.com>
2022-05-26 14:34:46 -0300

    get info from cli

--- wallet_client.py
+++ wallet_client.py
@@ -1,13 +1,16 @@
 import grpc
 import wallet_routes_pb2
 import wallet_routes_pb2_grpc
+import sys
 
 def wallet_get_balance(stub, wallet_id):
     balance = stub.GetBalance(wallet_routes_pb2.Wallet(id=wallet_id))
     return balance.total_balance
 
 def run():
-    with grpc.insecure_channel('localhost:42000') as channel:
+    wallet_id = sys.argv[1] or 'douglas_adams'
+    server_address = sys.argv[2] or 'localhost:42000'
+    with grpc.insecure_channel(server_address) as channel:
         stub = wallet_routes_pb2_grpc.WalletRoutesStub(channel)
         while True:
             wallet_id = input()
--- wallet_server.py
+++ wallet_server.py
@@ -2,6 +2,7 @@ from concurrent import futures
 import wallet_routes_pb2_grpc
 import wallet_routes_pb2
 import grpc
+import sys
 
 class WalletRoutesServicer(wallet_routes_pb2_grpc.WalletRoutesServicer):
     def GetBalance(self, request, context):
@@ -12,11 +13,13 @@ class WalletRoutesServicer(wallet_routes_pb2_grpc.WalletRoutesServicer):
             return wallet_routes_pb2.Balance(total_balance=balance)
 
 def serve():
+    port = sys.argv[1] or 42000
+    wallet_file = sys.argv[2] or 'wallets.txt'
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     wallet_routes_pb2_grpc.add_WalletRoutesServicer_to_server(
         WalletRoutesServicer(), server
     )
-    server.add_insecure_port('[::]:42000')
+    server.add_insecure_port(f'[::]:{port}')
     server.start()
     server.wait_for_termination()
 
